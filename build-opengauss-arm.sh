9f207a05d94029e262ea85cbd7cc9a03c56f1914
tuchief <tuchaonan@dataknown.cn>
2025-12-05 15:08:29 +0800

    feat(openeuler_open_gauss): 调整 ulimit 设置方式并修正构建平台参数
    - 将 ulimit 设置从 ENV 指令移至 entrypoint.sh 中执行，确保运行时生效
    - 注释掉 Dockerfile 中无效的 ENV ulimit 配置行
    - 修正 build-opengauss-arm.sh 脚本中的平台参数为 linux/arm64

--- build-opengauss-arm.sh
+++ build-opengauss-arm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # build openGauss arm64
-docker buildx build --platform linux/arm -f ./openeuler_open_gauss/Dockerfile -t server.aiknown.cn:31003/z-rps/opengauss:5.0.3_arm .
+docker buildx build --platform linux/arm64 -f ./openeuler_open_gauss/Dockerfile -t server.aiknown.cn:31003/z-rps/opengauss:5.0.3_arm .
--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -9,4 +9,11 @@ if [[ ! -f "$GAUSSHOME/data/isconfig" ]]; then
     chown -R omm:dbgrp /docker-entrypoint-initdb.d
     chmod -R 0700 /docker-entrypoint-initdb.d
 fi
+
+# -----------------------------
+# 设置 ulimit（运行时生效）
+# -----------------------------
+ulimit -n 1000000    # 最大打开文件数
+ulimit -u 65535      # 最大进程数（视需求可调）
+
 su omm -s /bin/bash -c '/bin/bash /opt/software/app.sh'
