0bf87517d6a0d68144fcad38559e8e8d84d4f150
tuchief <tuchaonan@dataknown.cn>
2026-01-05 10:27:23 +0800

    feat(database): 添加数据库创建日志输出
    - 在主节点初始化数据库时添加创建数据库语句的日志输出
    - 在初始化数据库时添加创建数据库语句的日志输出
    - 增强数据库初始化过程的可追溯性

--- image/app.sh
+++ image/app.sh
@@ -177,6 +177,7 @@ function init_db() {
                     echo -e "\033[32m ********************** docker entrypoint initdb *************************\033[0m" 
                     # master 初始化数据库            
                     if [ "$GAUSS_DATABASE" ]; then
+                        echo "CREATE DATABASE $GAUSS_DATABASE WITH OWNER $GAUSS_USER."
                         gsql -d postgres -c "CREATE DATABASE $GAUSS_DATABASE WITH OWNER = $GAUSS_USER ENCODING = 'UTF8' LC_COLLATE = 'C' LC_CTYPE = 'C' DBCOMPATIBILITY='PG' CONNECTION LIMIT = -1;"
                         # 字符串转小写
                         GAUSS_DATABASE=$(echo $GAUSS_DATABASE | tr 'A-Z' 'a-z')
@@ -220,6 +221,7 @@ function init_db() {
                 echo -e "\033[32m ********************** docker entrypoint initdb *************************\033[0m" 
                 # 初始化数据库            
                 if [ "$GAUSS_DATABASE" ]; then
+                    echo "CREATE DATABASE $GAUSS_DATABASE WITH OWNER $GAUSS_USER."
                     gsql -d postgres -c "CREATE DATABASE $GAUSS_DATABASE WITH OWNER = $GAUSS_USER ENCODING = 'UTF8' LC_COLLATE = 'C' LC_CTYPE = 'C' DBCOMPATIBILITY='PG' CONNECTION LIMIT = -1;"
                     # 字符串转小写
                     GAUSS_DATABASE=$(echo $GAUSS_DATABASE | tr 'A-Z' 'a-z')
